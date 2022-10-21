a77283381d89f29b2e6edcb6bcc8460f791ecbd1
EC2 Default User <ec2-user@ip-172-31-0-63.ec2.internal>
2022-10-21 10:26:41 +0000

    [Add]保存機能create　書き始め

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,11 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = book.new(list_params)
+    # データを受け取り新規登録するためのインスタンス作成
   end
 
   def index
