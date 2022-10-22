4a99698584bdd2e8261e449355f19479b83804ee
EC2 Default User <ec2-user@ip-172-31-0-63.ec2.internal>
2022-10-22 06:00:19 +0000

    [Add]バリデーション/エラーメッセージ[未完成]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,22 @@
 class BooksController < ApplicationController
   def new
     @book = Book.new
-  end
 
   def create
     # データを受け取り新規登録するためのインスタンス作成
-    @book = book.new(list_params)
+    @book = Book.new(book_params)
     # データをデータベースに保存するためのsaveメソッド実行
     # バリデーション/必須入力設定/投稿が成功した時に
     if @book.save
       flash[:notice] = "List was successfully created"
     # 詳細画面へリダイレクト
-      redirect_to book_path(@boo
+      redirect_to book_path(@book.id)
+    # 投稿が失敗した時newを表示
     else
       render :new
     end
   end
+
 
   def index
   end
