8e7abf7e4b785d6fde6d1e3d149155f80ccb7d1d
EC2 Default User <ec2-user@ip-172-31-0-63.ec2.internal>
2022-10-22 07:13:33 +0000

    一旦投稿createアクションをコメントアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,21 +4,25 @@ class BooksController < ApplicationController
 
   def create
     # データを受け取り新規登録するためのインスタンス作成
-    @book = Book.new(book_params)
+    # @book = Book.new(book_params)
+    book = Book.new(book_params)
     # データをデータベースに保存するためのsaveメソッド実行
     # バリデーション/必須入力設定/投稿が成功した時に
-    if @book.save
-      flash[:notice] = "List was successfully created"
-    # 詳細画面へリダイレクト
-      redirect_to book_path(@book.id)
+    # if @book.save
+    #   flash[:notice] = "List was successfully created"
+    book.save
+    # 投稿成功　 へリダイレクト
+      # redirect_to book_path(@book.id)
+     redirect_to '/'
     # 投稿が失敗した時newを表示
-    else
-      render :new
-    end
+  #   else
+  #     render :new
+  #   end
   end
 
 
   def index
+    @books = Book.all
   end
 
   def show
@@ -28,8 +32,10 @@ class BooksController < ApplicationController
   end
 
   private
+  
   # ストロングパラメータ
   def book_params
     params.require(:book).permit(:title, :body)
   end
+end
 end
