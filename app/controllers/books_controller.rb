a372c8410fe1f39ff2fbc7b7edd4ef5eccd87145
EC2 Default User <ec2-user@ip-172-31-0-63.ec2.internal>
2022-10-22 09:04:09 +0000

    [Update]一覧indexに詳細showへのリンク追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,8 +11,7 @@ class BooksController < ApplicationController
     if @book.save
       flash[:notice] = "List was successfully created"
     # 投稿成功　 へリダイレクト
-      redirect_to '/'
-      # 'book_path(@book.id)'
+      redirect_to 'book_path(@book.id)'
     # 投稿が失敗した時newを表示
     else
       render :new
