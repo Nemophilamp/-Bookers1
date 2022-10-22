b6bcdad033db051bc4a40aae784e6b7e177962a2
EC2 Default User <ec2-user@ip-172-31-0-63.ec2.internal>
2022-10-22 05:42:07 +0000

    [Add]バリデーション/サクセスメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,8 +4,17 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = book.new(list_params)
     # データを受け取り新規登録するためのインスタンス作成
+    @book = book.new(list_params)
+    # データをデータベースに保存するためのsaveメソッド実行
+    # バリデーション/必須入力設定/投稿が成功した時に
+    if @book.save
+      flash[:notice] = "List was successfully created"
+    # 詳細画面へリダイレクト
+      redirect_to book_path(@boo
+    else
+      render :new
+    end
   end
 
   def index
