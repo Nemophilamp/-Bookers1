426bc5f1269e4e26e190de2da3eedbfb6ee94c46
EC2 Default User <ec2-user@ip-172-31-0-63.ec2.internal>
2022-10-22 08:01:52 +0000

    一覧画面index　エラー出たまま

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,20 +4,17 @@ class BooksController < ApplicationController
 
   def create
     # データを受け取り新規登録するためのインスタンス作成
-    # @book = Book.new(book_params)
-    book = Book.new(book_params)
+    @book = Book.new(book_params)
     # データをデータベースに保存するためのsaveメソッド実行
     # バリデーション/必須入力設定/投稿が成功した時に
-    # if @book.save
-    #   flash[:notice] = "List was successfully created"
-    book.save
+    if @book.save
+      flash[:notice] = "List was successfully created"
     # 投稿成功　 へリダイレクト
-      # redirect_to book_path(@book.id)
-     redirect_to '/'
+      redirect_to book_path(@book.id)
     # 投稿が失敗した時newを表示
-  #   else
-  #     render :new
-  #   end
+    else
+      render :new
+    end
   end
 
 
@@ -32,7 +29,6 @@ class BooksController < ApplicationController
   end
 
   private
-  
   # ストロングパラメータ
   def book_params
     params.require(:book).permit(:title, :body)
